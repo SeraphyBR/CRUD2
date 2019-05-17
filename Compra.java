8bd078025765af59f97b09dc728cf55611a49bc7
HenriqueQuick <henrique.fernandes@sga.pucminas.br>
2019-05-17 07:57:23 -0300

    Update

--- Compra.java
+++ Compra.java
@@ -1,4 +1,6 @@
 import java.util.Date;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.text.*;
 
 class Compra{
@@ -29,12 +31,34 @@ class Compra{
         this.idCompra = id;
     }
 
-    
+    //Retorna um array de bytes com os bytes para escrever no arquivo
+    public byte[] toByteArray() throws Exception {
+        ByteArrayOutputStream dados = new ByteArrayOutputStream();
+        DataOutputStream saida = new DataOutputStream(dados);
+
+        saida.writeInt(this.idCliente);
+        saida.writeInt(this.idCompra);
+        saida.writeDate(this.dataCompra);
+        saida.writeFloat(this.valorCompra);
+        return dados.toByteArray();
+    }
+
+    //Coloca nos atributos os bytes lidos do arquivo
+    public void fromByteArray(byte[] b) throws Exception {
+        ByteArrayInputStream dados = new ByteArrayInputStream(b);
+        DataInputStream entrada = new DataInputStream(dados);
+
+        this.idCliente = entrada.readInt();
+        this.idCompra = entrada.readInt();
+        this.dataCompra = entrada.readDate();
+        this.valorCompra = entrada.readFloat();
+        entrada.close();
+    }
 
     public String toString(){
         return "Id Compra: " + this.idCompra + 
-                "Id Cliente: " + this.idCliente + 
-                "Data da compra: " + this.dataCompra.getTime() + 
-                "Valor da compra: " + this.valorCompra;
+                "\nId Cliente: " + this.idCliente + 
+                "\nData da compra: " + this.dataCompra.getTime() + 
+                "\nValor da compra: " + this.valorCompra;
        }
 }
--- ItemComprado.java
+++ ItemComprado.java
@@ -1,3 +1,48 @@
-public ItemComprado{
-    
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+class ItemComprado{
+
+    public int idCompra;
+    public int idProduto;
+    public int qtdProduto;
+    public float precoUnitario;
+
+    public ItemComprado(){
+        this.idCompra      = -1;
+        this.idProduto     = -1;
+        this.qtdProduto    = 0;
+        this.precoUnitario = 0.0;
+    }
+
+    //Retorna um array de bytes com os bytes para escrever no arquivo
+    public byte[] toByteArray() throws Exception {
+        ByteArrayOutputStream dados = new ByteArrayOutputStream();
+        DataOutputStream saida = new DataOutputStream(dados);
+
+        saida.writeInt(this.idCompra);
+        saida.writeInt(this.idProduto);
+        saida.writeInt(this.qtdProduto);
+        saida.writeFloat(this.precoUnitario);
+        return dados.toByteArray();
+    }
+
+    //Coloca nos atributos os bytes lidos do arquivo
+    public void fromByteArray(byte[] b) throws Exception {
+        ByteArrayInputStream dados = new ByteArrayInputStream(b);
+        DataInputStream entrada = new DataInputStream(dados);
+
+        this.idCompra = entrada.readInt();
+        this.idProduto = entrada.readInt();
+        this.qtdProduto = entrada.readInt();
+        this.precoUnitario = entrada.readFloat();
+        entrada.close();
+    }
+
+    public ItemComprado(int idCompra, int idProduto, int qtdProduto, flaot precoUnitario){
+        this.idCompra      = idCompra;
+        this.idProduto     = idProduto;
+        this.qtdProduto    = qtdProduto;
+        this.precoUnitario = precoUnitario;
+    }
 }
