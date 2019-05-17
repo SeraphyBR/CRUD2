edf3366a00fbbb472252663a389597d71ba58028
SeraphyBR <luisjuniorbr@gmail.com>
2019-05-17 07:52:58 -0300

    Update

--- Cliente.java
+++ Cliente.java
@@ -6,10 +6,58 @@ import java.io.*;
  * @author Breno
  * @author Henrique Fernandes
  * */
-public class Cliente implements Entidade
+class Cliente implements Entidade
 {
-    public static void main(String[] args) {
-        
+    protected int idCliente;
+    protected String nomeCliente;
+    protected String email;
 
+    //Contrutor vazio
+    public Cliente(){
+        this.idCliente = -1;
+        this.nomeCliente = "";
+        this.email = "";
     }
-}
+
+    //Construtor com parametros
+    public Cliente(String nomeCliente, String email){
+        this.nomeCliente = nomeCliente;
+        this.email = email;
+    }
+
+    public void setID(int id){
+        this.idCliente = id;
+    }
+
+    public int getID(){
+        return this.idCliente;
+    }
+
+    //Retorna um array de bytes com os bytes para escrever no arquivo
+    public byte[] toByteArray() throws Exception {
+        ByteArrayOutputStream dados = new ByteArrayOutputStream();
+        DataOutputStream saida = new DataOutputStream(dados);
+
+        saida.writeInt(this.idCliente);
+        saida.writeUTF(this.nomeCliente);
+        saida.writeUTF(this.email);
+        return dados.toByteArray();
+    }
+
+    //Coloca nos atributos os bytes lidos do arquivo
+    public void fromByteArray(byte[] b) throws Exception {
+        ByteArrayInputStream dados = new ByteArrayInputStream(b);
+        DataInputStream entrada = new DataInputStream(dados);
+
+        this.idCliente = entrada.readInt();
+        this.nomeCliente = entrada.readUTF();
+        this.email = entrada.readUTF();
+        entrada.close();
+    }
+
+    public String toString(){
+        return "Id:"        + this.idCliente +
+                "\nNome: "  + this.nomeCliente +
+                "\nEmail: " + this.email;
+    }
+}//End Cliente
